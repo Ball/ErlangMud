f4f2c91ed2505065a95ee6e5cd151ff64a111f3a
Brian J Ball <myotherpants@gmail.com>
2011-05-04 17:28:43 -0400

    Added movement tests to the application

--- src/stubs.erl
+++ src/stubs.erl
@@ -3,7 +3,7 @@
 -export([fake_rooms/0,stop_fake_rooms/0,login_with_rooms/0]).
 
 fake_rooms() ->
-        room:start_room(lobby, "Lobby", "It's lobby", [#room_exit{direction="north", description="an archway", location_key=kitchen}]),
+        room:start_room(lobby, "Lobby", "It's a lobby", [#room_exit{direction="north", description="an archway", location_key=kitchen}]),
         room:start_room(kitchen, "Kitchen", "It's a kitchen", [#room_exit{direction="south", description="dutch doors", location_key=lobby}]).
 stop_fake_rooms() ->
         gen_server:cast(lobby,stop),
--- test/player_tests.erl
+++ test/player_tests.erl
@@ -3,19 +3,34 @@
 
 player_test_() ->[
   ?Describe("Bad Password",
-    [?It("Should return an error",
+    [?It("Should return an error",fun setup/0,fun cleanup/1,
           ?_test(begin ?assertEqual(error, player:login("Tony", "BassPassword"))end))
     ]),
   ?Describe("Good Password",
-    [?It("Should have a player proxy",
-                    ?_test(begin ?assertEqual({ok,"You aint got jack!"},
-                                              (player:login("Tony", "Hello")):inventory())
+    [?It("Should have a player proxy",fun setup/0,fun cleanup/1,
+                    ?_test(begin Me = player:login("Tony", "Hello"),
+                                 ?assertEqual({ok,"You aint got jack!"},
+                                 Me:inventory())
                            end))
-     ])
+     ]),
+  ?Describe("Room Interaction",
+    [?It("It should describe the lobby",fun setup/0, fun cleanup/1,
+         ?_test(begin Me = player:login("Tony", "Hello"),
+                      ?assertEqual({ok, "It's a lobby"},
+                                   Me:look())
+                end)),
+     ?It("It should move to the kitchen", fun setup/0, fun cleanup/1,
+         ?_test(begin Me = player:login("Tony", "Hello"),
+                      Me:move("north"),
+                      ?assertEqual({ok, "It's a kitchen"},
+                                   Me:look()) end))])
 ].
 
 setup() ->
-  player:start_player(player_one).
+        % I don't know why, but I need the print
+        % to make the kitchen test pass
+        io:format(""),
+        stubs:fake_rooms().
 cleanup(_Pid) ->
-        player:stop_player(player_one),
+        stubs:stop_fake_rooms(),
         true.
