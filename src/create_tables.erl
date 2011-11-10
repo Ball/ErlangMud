c225abdb084d5758ec3cc857a122b5abdc2b70e0
Brian J Ball <myotherpants@gmail.com>
2011-11-10 11:14:06 -0500

    fixed table creation to be disc based

--- src/create_tables.erl
+++ src/create_tables.erl
@@ -5,5 +5,5 @@ drop_tables() ->
   mnesia:delete_table(player),
   mnesia:delete_table(room).
 init_tables() ->
-        mnesia:create_table(player, [{disc_copies, []}, {attributes, record_info(fields, player)}]),
-        mnesia:create_table(room,   [{disc_copies, []}, {attributes, record_info(fields, room)}]).
+    mnesia:create_table(player, [{disc_copies, [node()]}, {attributes, record_info(fields, player)}]),
+    mnesia:create_table(room,   [{disc_copies, [node()]}, {attributes, record_info(fields, room)}]).
